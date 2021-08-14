a992b829ba6d9a10fbf622e97750855ddfcf830d
agusfahmi <sensai.fahmi1@gmail.com>
2021-08-14 21:32:24 +0700

    r

--- script/main.js
+++ script/main.js
@@ -303,7 +303,7 @@ const resolveFetch = () => {
 };
 
 resolveFetch().then(animationTimeline());
-
+// audio
 var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
 if (!isChrome){
     $('#iframeAudio').remove()
